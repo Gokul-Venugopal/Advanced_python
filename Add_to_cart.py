dbdd14a0b0d378aff09aec2655bdcd5c153b5e75
Gokul-Venugopal <88612532+Gokul-Venugopal@users.noreply.github.com>
2021-08-18 21:48:28 +0530

    Add files via upload

---
+++ Add_to_cart.py
@@ -0,0 +1,3 @@
+import App                      #importing app as a Package
+
+App.add_to_cart_support()        #calling add to cart function
---
+++ App.py
@@ -0,0 +1,11 @@
+def open_home_page():
+    print("Welcome to the Home page")
+
+
+
+def add_to_cart_support():
+    print("Support for Add To Cart ")
+
+
+if __name__=='__main__':              #if main module is executed  open_home_page function is executed
+    open_home_page()
